27cc09eb53d338a07d8cd331e4f30eddae813266
HAMMER17 <plutalov70gmail.com>
2022-10-03 19:31:26 +0300

    weather

--- src/Loading.js
+++ src/Loading.js
@@ -9,6 +9,18 @@ const Loading = ({ data }) => {
     },
     Rain: {
       iconName: 'rainy',
+    },
+    Thunderstorm: {
+      iconName: 'thunderstorm',
+    },
+    Clear: {
+      iconName: 'sunny',
+    },
+    Drizzle: {
+      iconName: 'rainy',
+    },
+    Snow: {
+      iconName: 'snow',
     }
   }
   let item = JSON.parse(data)
